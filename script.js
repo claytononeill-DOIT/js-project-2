afa82819d5a64d76dbb8bcee4f6dba2c214b0aa2
O'Neill <coneill@towson.edu>
2024-10-18 12:08:03 -0400

    reorganized files and separated out content"

---
+++ script.js
@@ -0,0 +1,26 @@
+function compareDates() {
+  var currentDate = new Date();
+
+  dueDate = new Date(2024, 9, 17);
+  console.log(currentDate);
+  console.log(dueDate);
+
+  const diffInMS = dueDate.getTime() - currentDate.getTime();
+  const diffInSec = diffInMS / 1000;
+  const diffInMin = diffInSec / 60;
+  const diffInHrs = diffInMin / 60;
+  const diffInDays = diffInHrs / 24;
+  alert(
+    "there are " +
+      Math.floor(diffInDays) +
+      " days, " +
+      Math.floor(diffInHrs % 24) +
+      " hours, " +
+      Math.floor(diffInMin % 60) +
+      " minutes, and " +
+      Math.floor(diffInSec % 60) +
+      " seconds until October 17, 2024."
+  );
+}
+
+document.getElementById("alert-button").addEventListener("click", compareDates);
